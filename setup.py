b27a073a0931ddf237cac006eb6e689cd1e8f8e6
xoviat <xoviat@users.noreply.github.com>
2017-12-21 16:05:06 -0600

    Package properly

--- setup.py
+++ setup.py
@@ -4,13 +4,19 @@ from setuptools import setup
 
 setup(name="dewildcard",
       # packages = ['dewildcard'],
-      scripts = ['dewildcard'],
-      version = "0.1",
+      py_modules = ['dewildcard'],
+      entry_points = {
+        'console_scripts': [
+          'dewildcard = dewildcard:main',
+        ],
+      },
       description = "Expand wildcard imports in Python code",
       author = "Quentin Stafford-Fraser",
       author_email = "quentin@pobox.com",
       url = "http://github.com/quentinsf/dewildcard",
       download_url = 'https://github.com/quentinsf/dewildcard/tarball/0.1',
-      keywords = 'pylint'
+      keywords = 'pylint',
+      setup_requires = ['setuptools_scm'],
+      use_scm_version = True,
     )
 
